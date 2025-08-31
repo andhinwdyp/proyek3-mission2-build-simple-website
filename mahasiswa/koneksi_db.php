cb81c40e6cdc4d5ab74c76470b02a6a63301b18d
andhinwdyp <andhiniputri1806@gmail.com>
2025-09-01 05:45:56 +0700

    Delete mahasiswa directory

--- mahasiswa/koneksi_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $hostname = "localhost";
-    $username = "root";
-    $password = ""; 
-    $database = "mahasiswa";
-    
-    $koneksi = mysqli_connect($hostname, $username, $password, $database)or die('error'.mysqli_connect_error());
